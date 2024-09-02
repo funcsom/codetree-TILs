2bb045a99f0ab14fab8871efbec6ae5667d52b83
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-03 07:49:41 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240903/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int score;
+    cin >> score;
+
+    cout << "Your score is " << score << " Point.";
+    return 0;
+}
