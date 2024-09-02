a9af758e36c543087adc00f5a56811b765feacd0
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-03 08:45:18 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240903/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int z, x, y;
+    cin >> z;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+
+    cout << "010-" << y << "-" << x;
+    return 0;
+}
