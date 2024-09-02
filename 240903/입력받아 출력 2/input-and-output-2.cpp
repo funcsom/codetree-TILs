7864098cccb6a10243a1180ea65282d56a4412a3
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-03 08:41:15 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240903/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int front, back;
+    cin >> front;
+    cin.get();
+    cin >> back;
+
+    cout << front << back;
+    return 0;
+}
