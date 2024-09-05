ebefc43784a911dc143debedb4a482f9e423a35d
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-06 08:24:44 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240906/특정 숫자 맞추기/catching-specific-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    for (;;) {
+        cin >> n;
+        if (n < 25) {
+            cout << "Higher" << endl;
+        }
+        else if (n > 25) {
+            cout << "Lower" << endl;
+        }
+        else {
+            cout << "Good";
+            break;
+        }
+    }
+    return 0;
+}
