375ecfdc711c65109f3c0eab63aefa34e6a5c075
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-06 23:03:20 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240906/조건에 따른 연산/operatino-by-rule.cpp
+++ 240906/조건에 따른 연산/operatino-by-rule.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int n, cnt = 0;
+    // 변수 선언 및 입력
+    int n;
     cin >> n;
-
+    int cnt = 0;
+    
+    // n이 1000 미만일 때까지 반복
     while (n < 1000) {
-
-        if (n % 2 == 0) {
-            n = n * 2 + 2;
-            cnt++;
-        }
-        else {
-            n = n * 3 + 1;
-            cnt++;
-        }
-        if (n > 1000) cnt -= 1;
+        if (n % 2 == 1)
+            n = 2 * n + 2;
+        else
+            n = 3 * n + 1;
+        
+        cnt++;
     }
+    
     cout << cnt;
+    
     return 0;
 }
