beefe17f4ea2f1014b2ed368b87126eebfd08c37
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-08 00:40:46 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240908/별 그리기/star-drawing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt;
+    cin >> n;
+
+    cnt = 1;
+    for (int i = 0; i < 2 * n - 1; i++) {
+        for (int k = 0; k < n - cnt; k++) {
+            cout << ' ';
+        }
+        for (int j = 0; j < 2 * cnt - 1; j++) {
+            cout << '*';
+        }
+
+        if (i >= n - 1) {
+            cnt--;
+        }
+        else {
+            cnt++;
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
