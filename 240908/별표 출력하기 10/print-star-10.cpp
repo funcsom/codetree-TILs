8a1291887eb11e47ba6e76a5ba4dcb1ad436d8bd
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-08 01:04:49 +0900

    Title: 별표 출력하기 10, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240908/별표 출력하기 10/print-star-10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < 2 * n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < 1 + (i / 2); j++) {
+                cout << '*' << ' ';
+            }
+        }
+        else {
+            for (int k = 0; k < n - (i - 1) / 2; k++) {
+                cout << '*' << ' ';
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
