26140a2c92186843e5804cd9ab0b794e9f434f7b
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-07 10:41:13 +0900

    Title: 건너뛰고 별표 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240907/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << '*';
+        }
+        cout << endl << endl;
+    }
+    for (int k = 0; k < n - 1; k++) {
+        for (int l = 0; l < n - 1 - k; l++) {
+            cout << '*';
+        }
+        cout << endl << endl;
+    }
+    return 0;
+}
