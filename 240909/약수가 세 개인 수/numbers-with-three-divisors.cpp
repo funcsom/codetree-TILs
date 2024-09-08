35da78cd315299047c84da85de8fbbd01145ff83
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-09 08:23:23 +0900

    Title: 약수가 세 개인 수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240909/약수가 세 개인 수/numbers-with-three-divisors.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int start, end, cnt = 0;
+    cin >> start >> end;
+
+    for (int i = start; i <= end; i++) {
+        int a = 0;
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                a++;
+            }
+        }
+        if (a == 3) {
+            cnt++;
+        }
+    }
+    
+    cout << cnt;
+    return 0;
+}
