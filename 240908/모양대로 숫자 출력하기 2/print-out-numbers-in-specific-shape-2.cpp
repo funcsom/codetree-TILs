6a79fd853ba0613ce00fb700d7f669f0aa170f88
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-08 13:33:43 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240908/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt = 2;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << cnt << ' ';
+            if (cnt == 8) {
+                cnt = 2;
+                continue;
+            }
+            cnt += 2;
+        }
+        cout << endl;
+    }
+    return 0;
+}
