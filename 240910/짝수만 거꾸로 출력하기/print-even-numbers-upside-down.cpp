edc55ea16357a9c5aca87d92bb192cb33b0aa6e0
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-10 06:23:32 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240910/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.cpp
+++ 240910/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.cpp
@@ -9,8 +9,9 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
         if (arr[i] % 2 == 0) {
-            cnt++;
+
             where[cnt] = arr[i];
+            cnt++;
         }
     }
     for (int j = cnt - 1; j >= 0; j--) {
