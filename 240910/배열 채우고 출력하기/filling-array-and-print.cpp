2b248a8ad29456bd7c847cff9c24389b83a0fd64
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-10 06:01:50 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240910/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+
+    for (int j = 9; j >= 0; j--) {
+        cout << arr[j];
+    }
+    return 0;
+}
