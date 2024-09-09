65ce76767a01c28647fbfe36eabece081f7f804e
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-10 07:20:50 +0900

    Title: 배열에서 원소의 특징, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240910/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a[10];
+
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+        if (a[i] % 3 == 0) {
+            cout << a[i-1];
+            break;
+        }
+    }
+    return 0;
+}
