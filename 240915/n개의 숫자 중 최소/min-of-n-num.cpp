0d070652241bf993ac6bd514d75d8730b96ce8cc
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-15 21:45:35 +0900

    Title: n개의 숫자 중 최소, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240915/n개의 숫자 중 최소/min-of-n-num.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, arr[10], cnt=0, min_val;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (i == 0) {
+            min_val = arr[0];
+        }
+        else {
+            if (arr[i] <= min_val) {
+                min_val = arr[i];
+            }
+        }
+    }
+    
+    for (int j = 0; j < n; j++) {
+        if (arr[j] == min_val) {
+            cnt++;
+        }
+    }
+    cout << min_val << ' ' << cnt;
+    
+
+    return 0;
+}
