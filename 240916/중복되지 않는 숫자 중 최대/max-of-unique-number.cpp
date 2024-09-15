6b8551029e25ffb0eb472b4a163573f6317720af
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-16 00:16:33 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240916/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, a, arr[1001] = { 0, };
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        arr[a]++;
+    }
+
+    for (int j = 1; j <= n; j++) {
+        if (arr[j] == 1) {
+            cout << j;
+            break;
+        }
+    }
+    return 0;
+}
