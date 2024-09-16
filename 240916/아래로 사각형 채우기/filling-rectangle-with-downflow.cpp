5a908c065c362ce283b0ec508244a4a8a8adab37
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-16 09:52:07 +0900

    Title: 아래로 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, num;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        num = 1 + i;
+        for (int j = 0; j < n; j++) {
+            cout << num << ' ';
+            num += n;
+        }
+        cout << endl;
+    }
+    return 0;
+}
