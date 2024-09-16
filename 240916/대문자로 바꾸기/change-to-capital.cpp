d89c508c5d4762ee6d58c42f04761ff50c1878b6
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-16 09:28:14 +0900

    Title: 대문자로 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/대문자로 바꾸기/change-to-capital.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char arr_2d[5][3];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> arr_2d[i][j];
+            cout << (char)(arr_2d[i][j] - 32) << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
