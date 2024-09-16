5260aac72c1880665e640dec82ac284e8a959e7d
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-16 11:24:26 +0900

    Title: 동전이 있는 위치, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, arr[10][10] = {};
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int r, c;
+        cin >> r >> c;
+        arr[r-1][c-1] = 1;
+    }
+
+    for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) {
+            cout << arr[j][k] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
