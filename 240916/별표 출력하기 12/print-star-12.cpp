e9626fafcd53c27a8e869ee5bc26e58f7731b9cb
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-16 20:57:05 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240916/별표 출력하기 12/print-star-12.cpp
+++ 240916/별표 출력하기 12/print-star-12.cpp
@@ -12,7 +12,7 @@ int main() {
 
     if (n % 2 == 0) {
         for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n-1; j++) {
                 if (j % 2 == 1 && j - i >= 0) {
                     cout << '*' << ' ';
                 }
@@ -25,7 +25,7 @@ int main() {
     }
     else {
         for (int i = 0; i < n-2; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n-1; j++) {
                 if (j % 2 == 1 && j - i >= 0) {
                     cout << '*' << ' ';
                 }
