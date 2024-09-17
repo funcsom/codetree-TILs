35c06c7a87125b28b9cbfc1d0cc42174940be95b
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-17 21:12:36 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240917/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    if (str1.length() > str2.length()) {
+        cout << str1 << ' ' << str1.length();
+    }
+    else if (str2.length() > str1.length()) {
+        cout << str2 << ' ' << str2.length();
+    }
+    else {
+        cout << "same";
+    }
+    return 0;
+}
