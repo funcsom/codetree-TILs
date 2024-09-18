4e3dada895c1e69ab0e0bc5f43e6c5899a3d61fb
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-18 15:43:08 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240918/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
+++ 240918/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
@@ -6,6 +6,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string str[10];
     char c;
+    int cnt=0;
     for (int i = 0; i < 10; i++) {
         cin >> str[i];
     }
@@ -14,7 +15,11 @@ int main() {
     for (int i = 0; i < 10; i++) {
         if (str[i][str[i].length() - 1] == c) {
             cout << str[i] << endl;
+            cnt++;
         }
+    }
+    if (cnt == 0) {
+        cout << "None";
     }
     return 0;
 }
