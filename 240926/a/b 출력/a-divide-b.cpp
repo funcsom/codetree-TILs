dd340252a16607ea2898b5b1502c6e317fb36a15
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-26 10:36:33 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240926/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, temp;
+    cin >> a >> b;
+
+    // 정수자리수
+    if (a < b) {
+        cout << 0;
+    }
+    else {
+        while (a >= b) {
+            cout << a/b;
+            a = a/b;
+        }
+    }
+    cout << '.';
+
+    // 소수자리수
+    for (int i = 0; i < 20; i++) {
+        temp = (a % b) * 10 / b;
+        cout << temp;
+        a = (a % b) * 10 - (b * temp);
+        
+    }
+    return 0;
+}
