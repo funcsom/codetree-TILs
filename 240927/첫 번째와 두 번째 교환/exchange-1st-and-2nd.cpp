6c9edbc16c0b08744688ce03350216f051e908c4
성소민 <162946302+funcsom@users.noreply.github.com>
2024-09-27 09:47:03 +0900

    Title: 첫 번째와 두 번째 교환, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240927/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+    char first, second;
+    first = str[0];
+    second = str[1];
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == first) {
+            str[i] = second;
+        }
+        else if (str[i] == second) {
+            str[i] = first;
+        }
+    }
+
+    cout << str;
+    return 0;
+}
