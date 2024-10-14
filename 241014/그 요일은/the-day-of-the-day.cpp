17b95252089069f4b79d5506a2d73d22f54dac8c
성소민 <162946302+funcsom@users.noreply.github.com>
2024-10-14 10:26:24 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241014/그 요일은/the-day-of-the-day.cpp
+++ 241014/그 요일은/the-day-of-the-day.cpp
@@ -9,20 +9,27 @@ int main() {
 
     string week[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
-    for (int i = 0; i < 7; i++) {
-        if (day == week[i]) index = i + 1;
-    }
+        for (int i = 0; i < 7; i++) {
+            if (day == week[i]) index = i + 1;
+        }
 
-    int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    for (int i = m1 + 1; i < m2; i++) {
-        days += month[i];
+    if (m1 == m2) {
+        result = (d2 - d1) / 7;
+        if ((d2 - d1) % 7 >= index) result++;
+        cout << result;
     }
-    days += (month[m1] - d1 + 1);
-    days += d2;
+    else {
+        int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for (int i = m1 + 1; i < m2; i++) {
+            days += month[i];
+        }
+        days += (month[m1] - d1 + 1);
+        days += d2;
 
-    result = days / 7;
-    if (days % 7 >= index) result++;
-    cout << result;
+        result = days / 7;
+        if (days % 7 >= index) result++;
+        cout << result;
+    }
     // 여기에 코드를 작성해주세요.
     return 0;
 }
